7cbf01d233ee63a3e65fb84c020fd64ca4956276
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-09-20 15:27:17 +1000

    mods to deps versioning

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Cloudinaryex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8.0"},
+      {:httpoison, "~> 1.0.0"},
       {:poison, "~> 1.5.0"},
       {:timex, "~> 1.0.0-rc4"}
     ]
